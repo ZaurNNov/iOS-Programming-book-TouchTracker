69f06a8554f6434d62af4e697cdd42ab297615fe
Zaur Giyasov <zagiyas@mera.ru>
2018-04-25 16:37:26 +0300

    12  Touch Events and UIResponder - end
    Excerpt From: Conway, Joe. “iOS Programming: The Big Nerd Ranch Guide (4th Edition) (Big Nerd Ranch Guides).” iBooks.

--- TouchTracker/DrawView.m
+++ TouchTracker/DrawView.m
@@ -7,15 +7,112 @@
 //
 
 #import "DrawView.h"
+#import "Line.h"
+
+@interface DrawView()
+//@property (nonatomic, strong) Line *currentLine;
+@property (nonatomic, strong) NSMutableDictionary *linesInProgress;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+
+@end
 
 @implementation DrawView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark -
+
+-(instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.linesInProgress = [[NSMutableDictionary alloc] init];
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor lightGrayColor];
+        self.multipleTouchEnabled = YES;
+    }
+    
+    return self;
 }
-*/
+
+#pragma mark - Draw
+
+-(void)strokeLine: (Line *) line {
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
+- (void)drawRect:(CGRect)rect {
+    [[UIColor blackColor] set];
+    for (Line *line in self.finishedLines) {
+        [self strokeLine:line];
+    }
+    
+    [[UIColor redColor] set];
+    for (NSValue *key in self.linesInProgress) {
+        [self strokeLine:self.linesInProgress[key]];
+    }
+}
+
+#pragma mark - Touch
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        CGPoint location = [t locationInView:self];
+        Line *line = [[Line alloc] init];
+        line.begin = location;
+        line.end = location;
+        
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        self.linesInProgress[key] = line;
+    }
+
+    NSLog(@"-1- (void)touchesBegan event");
+    
+    [self setNeedsDisplay];
+}
+
+-(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        Line *line = self.linesInProgress[key];
+        line.end = [t locationInView:self];
+    }
+
+    NSLog(@"-2- (void)touchesMoved event");
+    [self setNeedsDisplay];
+}
+
+-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        Line *line = self.linesInProgress[key];
+        [self.finishedLines addObject:line];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+    
+    NSLog(@"-3- (void)touchesEnded event");
+    [self setNeedsDisplay];
+}
+
+- (void)touchesCancelled:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+    
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+    
+    NSLog(@"-4- (void)touchesCancelled event");
+    [self setNeedsDisplay];
+}
+
 
 @end
